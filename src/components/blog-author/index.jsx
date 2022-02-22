94863b1fd8529fae3adf676f92c2d55acba4652f
Yonanni <adhanamengisteab@gmail.com>
2022-02-22 21:30:02 +0100

    blog author

---
+++ src/components/blog-author/index.jsx
@@ -0,0 +1,23 @@
+import React, { Component } from "react";
+import { Row, Col, Image } from "react-bootstrap";
+import "./styles.css";
+export default class BlogAuthor extends Component {
+  state = {
+    verified: true // when signed in as author or admin, verified = true.
+  }
+  render() {
+    const { name, avatar } = this.props;
+    return (
+      <Row>
+        <Col xs={2}>
+          <Image className="blog-author" src={avatar} roundedCircle />
+        </Col>
+        <Col>
+          <div>by</div>
+          <h6>{name}</h6>
+          {/* on clicking edit, go to new page for changing avatar */}
+        </Col>
+      </Row>
+    );
+  }
+}
