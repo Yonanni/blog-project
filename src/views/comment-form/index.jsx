06ca8e873c36b0ac636e2792a59242c312c07a91
Yonanni <adhanamengisteab@gmail.com>
2022-03-04 11:10:26 +0100

    commentForm updated

--- src/views/comment-form/index.jsx
+++ src/views/comment-form/index.jsx
@@ -1,36 +1,41 @@
-import React, { Component } from "react";
+import React, { Component, useState } from "react";
 import { Form, Button } from "react-bootstrap";
 import "./styles.css";
 
-export default class CommentForm extends Component {
-    constructor(props) {
-      super(props);
-      this.state = {
-        name: "",
-        text: ""        
-      };
-    }
+const CommentForm = (props) =>  {
+    // constructor(props) {
+    //   super(props);
+    //   this.state = {
+    //     name: "",
+    //     text: ""        
+    //   };
+    // }
 
-    handleChange = e => {
+    const [comment, setComment] = useState({
+      name: "",
+      text: ""
+    })
+
+    const handleChange = e => {
         let id = e.target.id
-        this.setState({ ...this.state, [id]: e.target.value })
+        setComment({ ...comment, [id]: e.target.value })
     }
 
-    sendComment = async(e, id) => {
+    const sendComment = async(e, id) => {
         e.preventDefault()
-        console.log("Trying to send a new comment --> ", this.state)
+        console.log("Trying to send a new comment --> ", comment)
         try {
             let response = await fetch(`${process.env.REACT_APP_BE_URL}/blogs/${id}/comments`, { // trouble catching this postID
               method: 'POST',
-              body: JSON.stringify(this.state),
+              body: JSON.stringify(comment),
               headers: {
                 'content-type' : 'application/json'
             }
             })
             if (response.ok) {
-              console.log(response, this.state)
+              console.log(response, comment)
               console.log("NEW COMMENT POSTED")
-              this.props.reloadPage(e)
+              props.reloadPage(e)
             } else {
               alert("Something went wrong")
             }
@@ -39,28 +44,27 @@ export default class CommentForm extends Component {
           }
     }
 
-  render() {
-    console.log(this.state)
+
     return (
         <>
         <div className="mt-5"><strong>Tell us what you think!</strong></div>
 
-        <Form onSubmit={e => this.sendComment(e, this.props.post.id)}>
+        <Form onSubmit={e => sendComment(e, props.post.id)}>
             <div className="comment-form-container">
             <Form.Group controlId="name" className="my-3 mx-1">
                 <Form.Control 
                 size="lg" 
                 placeholder="Your name" 
-                value={this.state.name}
-                onChange={this.handleChange}
+                value={comment.name}
+                onChange={handleChange}
                 />
             </Form.Group>
             <Form.Group controlId="text" className="my-3 mx-1">
                 <Form.Control 
                 size="lg" 
                 placeholder="Your comment" 
-                value={this.state.text}
-                onChange={this.handleChange}
+                value={comment.text}
+                onChange={handleChange}
                 />
             </Form.Group>
             <Button
@@ -76,5 +80,7 @@ export default class CommentForm extends Component {
         </Form>
         </>
     );
-  }
+  
 }
+
+export default CommentForm
