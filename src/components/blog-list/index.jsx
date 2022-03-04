f6264b2ed16138d1375e00500c09b22a430cb862
Yonanni <adhanamengisteab@gmail.com>
2022-03-04 11:00:47 +0100

    blog lists done

--- src/components/blog-list/index.jsx
+++ src/components/blog-list/index.jsx
@@ -1,40 +1,40 @@
-import React, { Component } from "react";
+import React, { Component, useEffect, useState } from "react";
 import { Row, Col } from "react-bootstrap";
 import BlogItem from "../blog-item";
 import data from "../../data/posts.json"
-export default class BlogList extends Component { 
-  state = {
-    posts: data,
-    loading: false,
-  };
-  // componentDidMount() {
-  //   this.fetchBlogPosts();
-  // }
-  // fetchBlogPosts = async () => {
-  //   try {
-  //     let response = await fetch(
-  //       `${process.env.REACT_APP_BE_URL}/blogs`, {
-  //         method: 'GET',
-  //         headers: {
-  //           'content-type' : 'application/json'
-  //         }
-  //     });
-  //     let postsJson = await response.json();
-  //     this.setState({ posts: postsJson, loading: false })
-  //   } catch (error) {
-  //     console.log(`error fetching from ${process.env.REACT_APP_BE_URL}/blogs`)
-  //   }
-  // };
-  // componentDidUpdate(prevProps, prevState) {
-  // }
+const BlogList = () => { 
+ 
+  const [posts, setPosts] = useState(data)
+  const [loading, setLoading] = useState(false)
+
+  useEffect(()=> {
+   const fetchBlogPosts = async () => {
+      try {
+        let response = await fetch(
+          `${process.env.REACT_APP_BE_URL}/blogs`, {
+            method: 'GET',
+            headers: {
+              'content-type' : 'application/json'
+            }
+        });
+        let postsJson = await response.json();
+        this.setState({ posts: postsJson, loading: false })
+      } catch (error) {
+        console.log(`error fetching from ${process.env.REACT_APP_BE_URL}/blogs`)
+      }
+    };
+    // fetchBlogPosts()
+  },[])
+
+  
   
 
-  render() {
+  
     console.log("data", data)
     return (
-      this.state.loading === false ? (
+      loading === false ? (
         <Row>
-          {this.state.posts.map((post) => (
+          {posts.map((post) => (
           // {data.map((post) => (
             <Col md={4} key={post._id} style={{ marginBottom: 50 }}>
               <BlogItem {...post} />
@@ -43,7 +43,9 @@ export default class BlogList extends Component {
             
           ))}
         </Row>
-       ) : (<Row></Row>) 
+       ) : (<Row>Loading...</Row>) 
     );
-  }
+  
 }
+
+export default BlogList
